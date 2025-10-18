ce6d218e22008ec60d841b2f580d8321bc68a9bc
Ice-Thunderstorm <camden@live.com>
2025-10-17 21:19:26 -0700

    minor parallax changes

--- World/parallax_manager.gd
+++ World/parallax_manager.gd
@@ -5,11 +5,14 @@ extends Node2D
 @export var focus: Parallax2D
 @export var foreground: Parallax2D
 
+@export_category("Scroll Scale")
 @export var backgroundScrollScale: float = 1
 @export var foliageScrollScale: float = 0.2
 @export var focusScrollScale: float = 0.3
 @export var foregroundScrollScale: float = 1
 
+@export_category("Scroll Speed")
+@export var speedConst:float = 200
 @export var parallaxSpeed: float = 1
 
 # Called when the node enters the scene tree for the first time.
@@ -19,8 +22,8 @@ func _ready() -> void:
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	background.scroll_offset.x -= parallaxSpeed * backgroundScrollScale * 200 * delta
-	foliage.scroll_offset.x -= parallaxSpeed * foliageScrollScale * 200 * delta
-	focus.scroll_offset.x -= parallaxSpeed * focusScrollScale * 200 * delta
-	foreground.scroll_offset.x -= parallaxSpeed * foregroundScrollScale * 200 * delta
+	background.scroll_offset.x -= parallaxSpeed * backgroundScrollScale * speedConst * delta
+	foliage.scroll_offset.x -= parallaxSpeed * foliageScrollScale * speedConst * delta
+	focus.scroll_offset.x -= parallaxSpeed * focusScrollScale * speedConst * delta
+	foreground.scroll_offset.x -= parallaxSpeed * foregroundScrollScale * speedConst * delta
 	
